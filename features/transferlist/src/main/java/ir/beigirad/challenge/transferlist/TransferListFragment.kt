237948346a88e9283dbf86e27e3689bf2f3a70b7
Farhad Beigirad <f.beigirad@gmail.com>
2023-06-22 19:46:42 +0330

    add header section

--- features/transferlist/src/main/java/ir/beigirad/challenge/transferlist/TransferListFragment.kt
+++ features/transferlist/src/main/java/ir/beigirad/challenge/transferlist/TransferListFragment.kt
@@ -2,6 +2,7 @@ package ir.beigirad.challenge.transferlist
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.wada811.viewbinding.viewBinding
 import ir.beigirad.challenge.transferlist.databinding.TransferListLayoutBinding
@@ -15,5 +16,19 @@ class TransferListFragment : Fragment(R.layout.transfer_list_layout) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.appbar.btnSearch.setOnClickListener { showNotImplementedToast() }
+        binding.appbar.btnDownload.setOnClickListener { showNotImplementedToast() }
+        binding.appbar.btnNotification.setOnClickListener { showNotImplementedToast() }
+        binding.appbar.btnSupport.setOnClickListener { showNotImplementedToast() }
+
+        binding.tvBalance.text = getString(
+            R.string.transfer_list_balance_formatted,
+            "78,500,000"
+        )
+    }
+
+    private fun showNotImplementedToast() {
+        Toast.makeText(context, "NotImplemented", Toast.LENGTH_SHORT).show()
     }
 }
