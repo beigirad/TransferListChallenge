1e0e03ee9a7469109c369f03f8feefacae52b65e
Farhad Beigirad <f.beigirad@gmail.com>
2023-06-22 16:06:56 +0330

    initial project

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id(libs.plugins.androidApplication.get().pluginId)
+    id(libs.plugins.kotlinAndroid.get().pluginId)
+}
+
+android {
+    namespace = "ir.beigirad.challenge"
+    setCompileSdkVersion(33)
+
+    defaultConfig {
+        applicationId = "ir.beigirad.challenge"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.coreKtx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintLayout)
+
+    testImplementation(libs.jUnit)
+    androidTestImplementation(libs.androidJUnit)
+    androidTestImplementation(libs.espresso)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+rootProject.name = "Challenge"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("./gradle/libs.toml"))
+        }
+    }
+
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":app")
